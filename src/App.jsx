966356379804ffbb99432054a2f67942030955ad
remuwson <remigiuszablowski@gmail.com>
2025-07-24 09:10:37 +0200

    123

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
 
-      <h1>First Object</h1>
+      <h1>Calculator</h1>
+      <main>
+        <section className='Wynik'></section>
+        <div id='liczby1'>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div id='liczby2'>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div id='liczby3'>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div id='wynik'>
+          <button>0</button>
+          <button>Calculate</button>
+        </div>
+      </main>
       
     </>
   )
