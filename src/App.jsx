b9aafd6a3bfee1b8e73bb4125778aea9f0c2cb6c
remuwson <remigiuszablowski@gmail.com>
2025-07-24 10:02:29 +0200

    I add test.jsx and switch the web pages by react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Test from "../sites/test"; // zakładam że masz taki plik
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [currentPage, setCurrentPage] = useState("Counter");
+
   return (
-    <>
+    <Router>
+      <div>
+        <h1>Calculator</h1>
 
-      <h1>Calculator</h1>
-      <main>
-        <button onClick={() => setCurrentPage("Counter")}>Counter</button>
-        <button onClick={() => setCurrentPage("Description")}>Description</button>
-        <section className='Wynik'></section>
-        {currentPage === "Counter" && 
-        <div id='liczby1'>
-          <p>Number: {count}</p><br />
-          <button onClick={() => setCount(count + 1)}>+1</button>
-          <button onClick={() => setCount(count - 1)}>-1</button>
-          <button onClick={() => setCount(0)}>Reset</button>
-        </div>}
-        {currentPage === "Description" && 
-        <div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-        </div>}
-      </main>
+        {/* Nawigacja do podstron */}
+        <nav>
+          <Link to="/">Main Page</Link> |
+          <Link to="/test">Test</Link>
+        </nav>
 
-    </>
-  )
+        <Routes>
+          {/* Strona testowa */}
+          <Route path="/test" element={<Test />} />
+
+          {/* Strona główna – zawartość jak u Ciebie */}
+          <Route path="/" element={
+            <main>
+              <div id='buttons'>
+                <button onClick={() => setCurrentPage("Counter")}>Counter</button>
+                <button onClick={() => setCurrentPage("Description")}>Description</button>
+              </div>
+
+              {currentPage === "Counter" &&
+                <div id='liczby1'>
+                  <button id='numbers'>Number: {count}</button>
+                  <button onClick={() => setCount(count + 1)}>+1</button>
+                  <button onClick={() => setCount(count - 1)}>-1</button>
+                  <button onClick={() => setCount(0)}>Reset</button>
+                </div>}
+
+              {currentPage === "Description" &&
+                <div>
+                  <p>Lorem Ipsum is simply dummy text...</p>
+                  <p>Lorem Ipsum is simply dummy text...</p>
+                  <p>Lorem Ipsum is simply dummy text...</p>
+                </div>}
+            </main>
+          } />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
