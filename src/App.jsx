9726f5b36fcf5897f1e3f3e52fb9ee42ffb02988
remuwson <remigiuszablowski@gmail.com>
2025-07-24 09:31:22 +0200

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState("Counter");
   return (
     <>
 
       <h1>Calculator</h1>
       <main>
+        <button onClick={() => setCurrentPage("Counter")}>Counter</button>
+        <button onClick={() => setCurrentPage("Description")}>Description</button>
         <section className='Wynik'></section>
+        {currentPage === "Counter" && 
         <div id='liczby1'>
           <p>Number: {count}</p><br />
           <button onClick={() => setCount(count + 1)}>+1</button>
           <button onClick={() => setCount(count - 1)}>-1</button>
           <button onClick={() => setCount(0)}>Reset</button>
-        </div>
+        </div>}
+        {currentPage === "Description" && 
+        <div>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>}
       </main>
 
     </>
